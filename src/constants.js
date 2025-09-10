800d6a1e40bfeab28c4135b18a78133072cc4489
Garv Mittal <121634148+GarvMittal04@users.noreply.github.com>
2025-09-10 11:02:02 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyC-BMcjhAKxlTpq7ZJmM4YxQ6ucnn8ZZH0"
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
